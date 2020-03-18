3aea77506e0737d5f8659225938074dbe2793df2
WH-2099 <wh2099@outlook.com>
2020-03-19 00:56:11 +0800

    上传仓库

---
+++ amap.py
@@ -0,0 +1,135 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'WH-2099'
+
+import os
+import urllib.request
+import json
+import csv
+import datetime
+from tools.shellColor import printColorStr
+
+
+
+def showJson(data, pre=[]):
+    if isinstance(data, dict):
+        for k, v in data.items():
+            if isinstance(v, (dict,list)):
+                for subData in parsingGenerator(v, pre + [k]):
+                    yield subData
+            else:
+                yield (pre + [k], v)
+    elif isinstance(data, list):
+        for item in data:
+            if isinstance(item, (dict,list)):
+                for subData in parsingGenerator(item, pre):
+                    yield subData
+            else:
+                yield (pre, item)
+
+
+
+def getJson():
+    printColorStr('开始爬取\n', displayType='highlight')
+    url = 'http://wb.amap.com/channel.php?aoscommon=1&callback=_aosJsonpRequest1&urlname=https%3A%2F%2Fm5.amap.com%2Fws%2Fshield%2Fsearch%2Fyiqing&param=%5B%7B%22user_loc%22%3A%22%22%2C%22sign%22%3A1%7D%2C%7B%22fromchannel%22%3A%22gaode%22%2C%22version%22%3A4%2C%22first_request%22%3A1%2C%22sign%22%3A0%7D%5D&method=get'
+    flag = True
+    rawStr = None
+    while flag:
+        with urllib.request.urlopen(url) as response:
+            if response.status == 200:
+                flag = False
+                printColorStr('json文件获取中\n', foreColor='yellow', displayType='twinkle')
+                rawStr = response.read().decode()
+                printColorStr('json文件解码完成\n', foreColor='yellow', displayType='highlight')
+            else:
+                printColorStr('json文件获取失败，重试中\n', foreColor='red')
+    return rawStr
+
+
+
+def collectCityInfo(data):
+    rawList = data['citylist']
+    cityInfoList = []
+    cityInfo = {}
+    for province in rawList:
+        cityInfo['pName'] = province.get('name', None)
+        cityInfo['pId'] = province.get('id', None)
+        cityInfo['pTotal'] = province.get('total', None)
+        for city in province['list']:
+            cityInfo['name'] = city.get('name', None)
+            cityInfo['id'] = city.get('id', None)
+            cityInfo['lon'] = city.get('lon', None)
+            cityInfo['lat'] = city.get('lat', None)
+            cityInfo['level'] = city.get('level', None)
+            cityInfo['count'] = city.get('count', None)
+            #内容无意义
+            #cityInfo['textTitle'] = city.get('text_info', None).get('title',
+            #None)
+            #cityInfo['textContent'] = city.get('text_info',
+            #None).get('content', None)
+            cityInfoList.append(cityInfo.copy())
+    return cityInfoList
+
+
+
+def collectDate(data):
+    rawDate = data['date']
+    splitIndex = rawDate.find('月')  
+    year = 2020
+    month = int(rawDate[0:splitIndex])
+    day = int(rawDate[splitIndex + 1:-1])
+    date = datetime.date(year, month, day)
+    return date
+
+
+def collectPoisInfo(data):
+    rawList = data['pois']['one']['poilist'] + \
+              data['pois']['seven']['poilist'] + \
+              data['pois']['fourteen']['poilist'] + \
+              data['pois']['other']['poilist']
+    poisInfoList = []
+    poisInfo = {}
+    for poi in rawList:
+        poisInfo['poiname'] = poi.get('poiname', None)
+        poisInfo['lat'] = poi.get('lat', None)
+        poisInfo['lon'] = poi.get('lon', None)
+        #内容无意义
+        #poisInfo['number'] = poi.get('number', None)
+        #poisInfo['tag'] = poi.get('tag_display_std', None)
+        poisInfo['source'] = poi.get('source', None)
+        poisInfoList.append(poisInfo.copy())
+    return poisInfoList
+
+
+
+def readJson(rawStr):
+    begin = rawStr.find('{')
+    end = rawStr.rfind('}') + 1
+    jsonStr = rawStr[begin:end]
+    rawData = json.loads(jsonStr)
+    data = rawData['data']
+    date = collectDate(data)
+    cityInfoList = collectCityInfo(data)
+    poisInfoList = collectPoisInfo(data)
+    return (date, cityInfoList, poisInfoList)
+
+
+
+def saveCsv(dataList, file):
+    with open(file, 'w', encoding='gb18030', newline='') as csvF:  
+        #使用csv模块读写文件，打开时需要设置参数 newline=''
+        #gb18030保证excel直接读取
+        heads = dataList[0].keys()
+        writer = csv.DictWriter(csvF,heads)
+        writer.writeheader()
+        writer.writerows(dataList)
+    
+
+
+if __name__ == '__main__':
+    jsonStr = getJson()
+    date, cityInfoList, poisInfoList = readJson(jsonStr)
+    printColorStr(f'{date!s}数据读取成功\n', foreColor='green')    
+    saveCsv(cityInfoList, 'data/City.csv')
+    saveCsv(poisInfoList, 'data/Pois.csv')
+    printColorStr('!!!导出csv成功!!!\n\n\n', foreColor='green', displayType='highlight')
---
+++ tools/shellColor.py
@@ -0,0 +1,148 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'命令行输出颜色'
+
+__author__ = 'WH-2099'
+
+
+import os
+import ctypes
+
+
+
+def _posixPrintColorStr(string:str, foreColor:str, backColor:str, displayType:str):
+    '''
+    ANSI转义序列--适用于POSIX
+    格式：\033[显示方式;前景色;背景色m
+    说明：
+    前景色            背景色           颜色
+    ---------------------------------------
+    30                40              黑色
+    31                41              红色
+    32                42              绿色
+    33                43              黃色
+    34                44              蓝色
+    35                45              紫红色
+    36                46              青蓝色
+    37                47              白色
+    显示方式           意义
+    ---------------------------------------
+    0                终端默认设置
+    1                高亮显示
+    4                使用下划线
+    5                闪烁
+    7                反白显示
+    8                不可见
+    '''
+    if displayType:
+        displayType = {'default':  '0',
+                       'highlight':'1',
+                       'underline':'4',
+                       'twinkle':  '5',
+                       'swap':     '7',
+                       'invisible':'8'}.get(displayType, '0')
+    if foreColor:
+        foreColor = {'black': '30',
+                     'red':   '31',
+                     'green': '32',
+                     'yellow':'33',
+                     'blue':  '34',
+                     'purple':'35',
+                     'cyan':  '36',
+                     'white': '37',
+                     'default': ''}.get(foreColor, '')
+    if backColor:
+        backColor = {'black': '40',
+                     'red':   '41',
+                     'green': '42',
+                     'yellow':'43',
+                     'blue':  '44',
+                     'purple':'45',
+                     'cyan':  '46',
+                     'white': '47',
+                     'default': ''}.get(backColor, '')
+    #filter是为了去除空字符串，防止生成连续的冒号';;'，连续冒号将使转义序列失效
+    string = '\033[' + ';'.join(filter(None,(displayType, foreColor, backColor))) + 'm' + string + '\033[0m'
+    print(string, end='', flush=True)
+
+
+
+def _windowsPrintColorStr(string:str, foreColor:str, backColor:str, displayType:str):
+    '''
+    cmd32设置输出颜色--适用于windows(nt)
+    利用SetConsoleTextAttribute函数
+    靠一个字节的低四位控制前景色，高四位控制背景色
+    即由2位十六进制组成，分别取0~f
+    前一位指的是背景色，后一位指的是字体色
+    组合使用[或]运算
+    '''
+    STD_INPUT_HANDLE = -10
+    STD_OUTPUT_HANDLE = -11
+    STD_ERROR_HANDLE = -12
+    std_out_handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+    #除默认颜色外，基本颜色相同，低四位前景色，高四位背景色
+    SINGLE_COLOR = {'black': 0x0,
+                    'blue':  0x1,
+                    'green': 0x2,
+                    'cyan':  0x3,
+                    'red':   0x4,
+                    'purple':0x5,
+                    'yellow':0x6,
+                    'white' :0x7,
+                    'highlight':0x8}
+    #前景色默认为white，背景色默认为black，即0x07
+    defaultColor= 0x07
+    foreColor = SINGLE_COLOR.get(foreColor, SINGLE_COLOR['white'])
+    backColor = SINGLE_COLOR.get(backColor, SINGLE_COLOR['black'])
+    #高亮处理
+    if displayType == 'highlight':
+        foreColor |= SINGLE_COLOR['highlight']
+    if displayType == 'twinkle':
+        backColor |= SINGLE_COLOR['highlight']
+    #背景色需要左移四位
+    backColor <<= 4
+    color = foreColor | backColor
+    ctypes.windll.kernel32.SetConsoleTextAttribute(std_out_handle, color)
+    print(string, end='', flush=True)
+    ctypes.windll.kernel32.SetConsoleTextAttribute(std_out_handle, defaultColor)
+
+    
+
+def printColorStr(string:str, foreColor:str='', backColor:str='', displayType:str=''):
+    '''printColorStr(string:str, foreColor:str='', backColor:str='', displayType:str='')
+    以指定颜色格式输出字符串，自动匹配平台
+    string:要输出的字符串
+    foreColor:前景色
+    backColor:背景色
+    displayType:显示类型
+
+    Color：'black', 'blue', 'green', 'cyan', 'red', 'purple', 'yellow', 'white'
+    displayType:'default', 'highlight', 'underline', 'twinkle', 'swap', 'invisible'
+    
+    ###displayType具体实现受限于终端及平台类型，可能并不生效
+    ###但经测试highlight在各种情况下表现良好
+    '''
+    if os.name == 'nt':
+        _windowsPrintColorStr(string, foreColor, backColor, displayType)
+    elif os.name == 'posix':
+        _posixPrintColorStr(string, foreColor, backColor, displayType)
+    else:
+        raise SystemError
+
+
+
+if __name__ == '__main__':
+    '输出全部支持效果'
+    supportColor=('black', 'blue', 'green', 'cyan', 'red', 'purple', 'yellow', 'white')
+    supportDisplayType=('default', 'highlight', 'underline', 'twinkle', 'swap', 'invisible')
+    for displayType in supportDisplayType:
+        print(f"{displayType:^30}")
+        for foreColor in supportColor:
+            for backColor in supportColor:
+                Test=f"{foreColor[:1]}{backColor[:1]}{displayType[:1]}"
+                printColorStr(Test, foreColor, backColor, displayType)
+                print(' ', end='', flush=True)
+            else:
+                print()
+        else:
+            print()
