33a24ef6a0cf77f5ccd5dfe7e61175bcb66ad941
WH-2099 <wh2099@outlook.com>
2020-04-02 15:03:39 +0800

    增加患者轨迹描述

--- crawler.py
+++ crawler.py
@@ -11,16 +11,17 @@ import os
 import random
 
 import requests
+import requests_html
 import yaml
 
 
 
 class Crawler:
     '''
-    Crawler(self, logger=None, session=None, encoding='ut-8')
+    Crawler(self, logger=None, session=None)
     基本爬虫类
     '''
-    def __init__(self, logger=None, session=None, encoding='ut-8'):
+    def __init__(self, logger=None, session=None):
         if not logger:
             logger = logging.getLogger(__name__)
         if not session:
@@ -28,7 +29,6 @@ class Crawler:
 
         self.logger = logger
         self.session = session
-        self.encoding = encoding
 
         self.rawData = None
         self.datasDict = {}
@@ -43,13 +43,20 @@ class Crawler:
                         yield subData
                 else:
                     yield (pre + [k], v)
-        elif isinstance(data, (list, tuple)):
+        elif isinstance(data, list):
             for item in data:
                 if isinstance(item, (dict, list, tuple)):
                     for subData in showJson(item, pre):
                         yield subData
                 else:
-                    yield (pre, item)
+                    yield (pre + ['[,]'], item)
+        elif isinstance(data, tuple):
+            for item in data:
+                if isinstance(item, (dict, list, tuple)):
+                    for subData in showJson(item, pre):
+                        yield subData
+                else:
+                    yield (pre + ['(,)'], item)
 
 
     def getJson(self, url, **kargs):
@@ -62,7 +69,6 @@ class Crawler:
         with self.session.get(url, **kargs) as response:
             if response.status_code == requests.codes.ok:
                 self.logger.info('json爬取成功')
-                response.encoding = self.encoding
                 rawStr = response.text
             else:
                 self.logger.error('json爬取失败')
@@ -87,7 +93,7 @@ class Crawler:
         '''
         pass
 
-    
+
     def saveCsv(self, dir, **kargs):
         '''
         保存csv文件
@@ -97,7 +103,7 @@ class Crawler:
             file = os.path.join(dir, str(tableName) + '.csv')
             self.logger.info(file + '输出开始')
             try:
-                with open(file, 'w', newline='', **kargs) as csvF:  
+                with open(file, 'w', newline='', **kargs) as csvF:
                 #csv模块官方要求打开时设置参数 newline=''
                     heads = datas[0].keys()
                     writer = csv.DictWriter(csvF, heads)
@@ -117,11 +123,10 @@ class AmapCrawler(Crawler):
 
     url = 'http://wb.amap.com/channel.php?aoscommon=1&callback=_aosJsonpRequest1&urlname=https%3A%2F%2Fm5.amap.com%2Fws%2Fshield%2Fsearch%2Fyiqing&param=%5B%7B%22user_loc%22%3A%22%22%2C%22sign%22%3A1%7D%2C%7B%22fromchannel%22%3A%22gaode%22%2C%22version%22%3A4%2C%22first_request%22%3A1%2C%22sign%22%3A0%7D%5D&method=get'
 
-    def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
 
-    def getJson(self):
-        return super().getJson(self.url)
+    def getJson(self, **kargs):
+        super().getJson(self.url, **kargs)
+
 
     def parseRawData(self):
         datas = self.rawData['data']
@@ -130,12 +135,10 @@ class AmapCrawler(Crawler):
         poisInfoList = self.__collectPoisInfo(datas)
         self.datasDict['City'] = cityInfoList
         self.datasDict['Pois'] = poisInfoList
-        
 
 
-
-    def __collectCityInfo(self, data):
-        rawList = data['citylist']
+    def __collectCityInfo(self, datas):
+        rawList = datas['citylist']
         cityInfoList = []
         cityInfo = {}
         for province in rawList:
@@ -158,10 +161,9 @@ class AmapCrawler(Crawler):
         return cityInfoList
 
 
-
-    def __collectDate(self, data):
-        rawDate = data['date']
-        splitIndex = rawDate.find('月')  
+    def __collectDate(self, datas):
+        rawDate = datas['date']
+        splitIndex = rawDate.find('月')
         year = 2020
         month = int(rawDate[0:splitIndex])
         day = int(rawDate[splitIndex + 1:-1])
@@ -169,11 +171,8 @@ class AmapCrawler(Crawler):
         return date
 
 
-    def __collectPoisInfo(self, data):
-        rawList = data['pois']['one']['poilist'] + \
-                  data['pois']['seven']['poilist'] + \
-                  data['pois']['fourteen']['poilist'] + \
-                  data['pois']['other']['poilist']
+    def __collectPoisInfo(self, datas):
+        rawList = datas['pois']['one']['poilist'] + datas['pois']['seven']['poilist'] + datas['pois']['fourteen']['poilist'] + datas['pois']['other']['poilist']
         poisInfoList = []
         poisInfo = {}
         for poi in rawList:
@@ -189,12 +188,45 @@ class AmapCrawler(Crawler):
 
 
 
+class UcCrawler(Crawler):
+    '''
+    UC爬虫
+    '''
 
-        
+    url = 'https://iflow-api.uc.cn/feiyan/track?page=0&size=100000&fallback=1&loc=%E5%85%A8%E9%83%A8%2C%E5%85%A8%E9%83%A8'
+
+
+    def getJson(self, **kargs):
+        super().getJson(self.url, **kargs)
+
+
+    def parseRawData(self):
+        datas = self.rawData['data']
+        TrackeInfoList = self.__collectTrackeInfo(datas)
+        self.datasDict['Tracke'] = TrackeInfoList
+
+
+    def __collectTrackeInfo(self, datas):
+        trackes=datas['trackes']
+        TrackeInfoList=[]
+        TrackeInfo={}
+        for Tracke in trackes:
+            TrackeInfo['id']=Tracke.get('id', None)
+            TrackeInfo['province']=Tracke.get('province', None)
+            TrackeInfo['city']=Tracke.get('city', None)
+            TrackeInfo['base_info']=Tracke.get('base_info', None)
+            TrackeInfo['detail_info']=Tracke.get('detail_info', None)
+            #内容无意义
+            #TrackeInfo['index']=Tracke.get('index', None)
+            TrackeInfo['source']=Tracke.get('source', None)
+            TrackeInfo['is_from_outside']=Tracke.get('is_from_outside', None)
+            TrackeInfoList.append(TrackeInfo.copy())
+        return TrackeInfoList
+
 
 
 if __name__ == '__main__':
-    
+
     with open('config/LogConfigNormal.yaml', 'r', encoding='utf-8') as configYaml:
         configDict = yaml.safe_load(configYaml)
         logging.config.dictConfig(configDict)
@@ -207,8 +239,12 @@ if __name__ == '__main__':
         session.headers['user-agent'] = ua['String']
 
     amap = AmapCrawler(logger, session)
-    amap.getJson()
+    amap.getJson(timeout=10)
     amap.parseRawData()
     amap.saveCsv('data/', encoding='gb18030')
 
-    
+    uc = UcCrawler(logger, session)
+    uc.getJson(timeout=10)
+    uc.parseRawData()
+    uc.saveCsv('data/', encoding='gb18030')
+
